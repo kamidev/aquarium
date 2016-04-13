46a2922f1786d6a178459fb9083ef8f82f7c81e8
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2016-04-13 19:12:07 +0200

    Frontend mock

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,9 @@
 defmodule Aquarium.PageController do
   use Aquarium.Web, :controller
+  alias Aquarium.WorldState
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html", fish: %{ :tangerine => {3, 4} }, min_cell: 0, max_cell: 9
   end
+
 end
--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,3 +1,9 @@
 defmodule Aquarium.PageView do
   use Aquarium.Web, :view
+  alias Aquarium.Fish
+
+  def content(fish, x, y) do
+    Enum.find_value(fish, "empty", fn {name, place} -> if place == {x, y} do name end end)
+  end
+
 end
