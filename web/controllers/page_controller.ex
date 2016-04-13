d2d7eb135a68f94f4fb033ad711cacacba93d5af
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2016-04-13 19:18:12 +0200

    Deleted some stuff not to be seen

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,6 +1,5 @@
 defmodule Aquarium.PageController do
   use Aquarium.Web, :controller
-  alias Aquarium.WorldState
 
   def index(conn, _params) do
     render conn, "index.html", fish: %{ :tangerine => {3, 4} }, min_cell: 0, max_cell: 9
