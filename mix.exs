dee3a55abe3522d9de0192a2dd06dac9793d545f
kamidev <jonas.beckman@gmail.com>
2019-05-29 00:53:00 +0200

    Update Erlang, Elixir and Phoenix to latest

--- mix.exs
+++ mix.exs
@@ -3,13 +3,13 @@ defmodule Aquarium.Mixfile do
 
   def project do
     [app: :aquarium,
-     version: "0.0.1",
-     elixir: "~> 1.3.1",
+     version: "0.0.4",
+     elixir: "~> 1.8.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps()]
   end
 
   # Configuration for the OTP application.
@@ -29,13 +29,14 @@ defmodule Aquarium.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.2.0"},
-      {:phoenix_pubsub, "~> 1.0"},
-      {:phoenix_html, "~> 2.6"},
-      {:phoenix_live_reload, "~> 1.0", only: :dev},
-      {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"},
-      {:credo, "~> 0.4.5", only: [:dev, :test]}
+      {:phoenix, "~> 1.4.6"},
+      {:phoenix_pubsub, "~> 1.1.2"},
+      {:phoenix_html, "~> 2.13.2"},
+      {:phoenix_live_reload, "~> 1.2.1", only: :dev},
+      {:gettext, "~> 0.16.1"},
+      {:plug_cowboy, "~> 2.0.2"},
+      {:jason, "~> 1.1.2"},
+      {:credo, "~> 1.0.5", only: [:dev, :test]}
     ]
   end
 end
