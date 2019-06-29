e246fb02b45b48d7454cb3d880361143e2e638e9
kamidev <jonas.beckman@gmail.com>
2019-06-29 10:51:02 +0200

    Fix conflict

--- mix.exs
+++ mix.exs
@@ -2,27 +2,28 @@ defmodule Aquarium.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :aquarium,
-     version: "0.0.4",
-     elixir: "~> 1.8.0",
-     elixirc_paths: elixirc_paths(Mix.env),
-     compilers: [:phoenix, :gettext] ++ Mix.compilers,
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :aquarium,
+      version: "0.0.4",
+      elixir: "~> 1.8.0",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application.
   #
   # Type `mix help compile.app` for more information.
   def application do
-    [mod: {Aquarium, []},
-     applications: [:phoenix, :phoenix_html, :cowboy, :logger, :gettext]]
+    [mod: {Aquarium, []}, applications: [:phoenix, :phoenix_html, :cowboy, :logger, :gettext]]
   end
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
-  defp elixirc_paths(_),     do: ["lib", "web"]
+  defp elixirc_paths(_), do: ["lib", "web"]
 
   # Specifies your project dependencies.
   #
@@ -33,7 +34,7 @@ defmodule Aquarium.Mixfile do
       {:phoenix_pubsub, "~> 1.1.2"},
       {:phoenix_html, "~> 2.13.2"},
       {:phoenix_live_reload, "~> 1.2.1", only: :dev},
-      {:gettext, "~> 0.16.1"},
+      {:gettext, "~> 0.17.0"},
       {:plug_cowboy, "~> 2.1.0"},
       {:jason, "~> 1.1.2"},
       {:credo, "~> 1.1.0", only: [:dev, :test]}
